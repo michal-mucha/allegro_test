938ff9cb8d515092516c70e440fc0d0a9df55bd8
Michal <m.m@s.pl>
2022-06-04 12:16:02 +0200

    python script for creating dockerfile from tamplate

--- pipelines/python_scripts/create_dockerfile_from_template.py
+++ pipelines/python_scripts/create_dockerfile_from_template.py
@@ -6,9 +6,12 @@ if __name__ == "__main__":
     path_of_final_docker_file = sys.argv[2]
     service_name = sys.argv[3]
     if service_name is None or docker_file_template_path is None or path_of_final_docker_file is None:
-        raise Exception("Required arguments not passed")
+        raise Exception("Required arguments not passed. Provide arguments in proper order and try again")
+
+    print(f'Creating of Dockerfile for service: {service_name} started')
     final_docker_file_content = []
     service_pointer_pattern = '{{service_name_pointer}}'
+    print(f'Reading Dockerfile from path: {docker_file_template_path}')
     with open(docker_file_template_path, "r") as dockerfile_content:
         for line in dockerfile_content.readlines():
             if re.search(service_pointer_pattern, line) is not None:
@@ -17,5 +20,6 @@ if __name__ == "__main__":
                 continue
             final_docker_file_content.append(line)
 
+    print(f'Writing Docker file to {path_of_final_docker_file}')
     with open(path_of_final_docker_file, 'w') as docker_file:
         docker_file.write("\n".join(final_docker_file_content))
